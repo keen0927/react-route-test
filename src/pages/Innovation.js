a11e9b608a529423763c753bc7a074fa3f4ddd5a
KEEN <kyh0927@live.co.kr>
2019-03-16 00:06:11 +0900

    first commit

---
+++ src/pages/Innovation.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Innovation extends Component {
+    render() {
+        return (
+            <div>
+                Innovation
+            </div>
+        );
+    }
+}
+
+export default Innovation;
